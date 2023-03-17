66ede71dc37daad8d3e5df9088c6ac5be662e98a
Jorge Sisco <jorgesisco17@gmail.com>
2023-03-17 09:59:37 +0100

    Teste the Project routes

--- tests/test_projects.py
+++ tests/test_projects.py
@@ -7,15 +7,41 @@ def test_create_project(client):
     project_data = {"name": "Test Project", "description": "A test project"}
     response = client.post("/projects", json=project_data)
 
-    assert response.status_code == status.HTTP_201_CREATED
+    assert response.status_code == 200
     assert response.json()["name"] == project_data["name"]
     assert response.json()["description"] == project_data["description"]
 
-# def test_get_project(client):
-#     # TODO: Implement the get project test
+    client.delete(f'/projects/{response.json()["id"]}')
+
+
+def test_get_project(client):
+    response = client.get("/projects")
+    assert response.status_code == 200
+
+
 #
-# def test_update_project(client):
-#     # TODO: Implement the update project test
+def test_update_project(client):
+    project_data = {"name": "Test Project", "description": "A test project"}
+    updated_project_data = {"name": "Test Project 2", "description": "An updated test project"}
+
+    post_response = client.post("/projects", json=project_data)
+
+    response = client.put(f'/projects/{post_response.json()["id"]}', json=updated_project_data)
+
+    assert response.status_code == 200
+    assert response.json()["name"] == updated_project_data["name"]
+    assert response.json()["description"] == updated_project_data["description"]
+
+    client.delete(f'/projects/{response.json()["id"]}')
+
+
 #
-# def test_delete_project(client):
-#     # TODO: Implement the delete project test
+def test_delete_project(client):
+    project_data = {"name": "Test Project", "description": "A test project"}
+    post_response = client.post("/projects", json=project_data)
+
+    response = client.delete(f'/projects/{post_response.json()["id"]}')
+
+    assert response.status_code == 200
+    assert response.json()["name"] == project_data["name"]
+    assert response.json()["description"] == project_data["description"]
